4ebab865c83d443db5be7d2b25fbaf1ad91299db
Julian S <erasedot@gmail.com>
2018-01-17 19:27:00 -0500

    remove shopify wut?

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var axios = require('axios');
-var client = require('../shopify');
+// var client = require('../shopify');
 require('dotenv').config()
 
 
@@ -13,15 +13,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Monique Michele Deliz' });
 });
 
-router.get('/products', function(req,res,next) {
-  client.productListing.list(function(products){
-    console.log(products)
-  })
-});
-
-router.get('/products/{id}', function(req,res,next) {
-
-});
+// router.get('/products', function(req,res,next) {
+//   client.productListing.list(function(products){
+//     console.log(products)
+//   })
+// });
+//
+// router.get('/products/{id}', function(req,res,next) {
+//
+// });
 
 
 module.exports = router;
